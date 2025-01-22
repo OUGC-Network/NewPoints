68a503968feef6af62074296ae5ba41f57830eb9
Sama34 <Sama34@users.noreply.github.com>
2025-01-22 13:55:02 -0800

    Fix `language_load()` missing

--- Upload/admin/modules/newpoints/module_meta.php
+++ Upload/admin/modules/newpoints/module_meta.php
@@ -40,7 +40,11 @@ function newpoints_meta(): bool
 {
     global $page, $lang;
 
-    language_load();
+    if (function_exists('\Newpoints\Core\language_load')) {
+        language_load();
+    } else {
+        $lang->load('newpoints_module_meta');
+    }
 
     $sub_menu_items = [
         10 => [
