8a0c711b1162d696ed1db7fbb90b634273d0782d
Sama34 <Sama34@users.noreply.github.com>
2025-02-03 01:11:21 -0800

    META 3.1.2

--- Upload/inc/plugins/newpoints.php
+++ Upload/inc/plugins/newpoints.php
@@ -66,9 +66,9 @@ use function Newpoints\Core\users_update;
 
 use const Newpoints\ROOT;
 
-const NEWPOINTS_VERSION = '3.1.1';
+const NEWPOINTS_VERSION = '3.1.2';
 
-const NEWPOINTS_VERSION_CODE = 3101;
+const NEWPOINTS_VERSION_CODE = 3102;
 
 const MAX_DONATIONS_CONTROL = 5; // Maximum donations someone can send each 15 minutes
 
